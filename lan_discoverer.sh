8eb7cadee1945ca48bdf81d50af41a87335bc332
Arturo Ruiz Mañas <arruma2160@gmail.com>
2016-09-03 14:10:32 +0100

    Output of errors in stderr and retrieving netmask on success

--- lan_discoverer.sh
+++ lan_discoverer.sh
@@ -4,22 +4,36 @@
 #   @arg:       <network_interface> e.g eth0, wlan0, lo
 #   @example:   get_netmask eth0
 #   @return:    error -> 1
+#               success -> string:network mask address
 get_netmask () {
     
     ## Verifying passing of one argument - just considering the first argument
     if [ ! "$1" ]; then
-        echo "[ *] Error - 'get_netmask' usage: "
-        echo -e "\tget_netmask <network_interface>"
+        ( >&2 echo "[ *] Error - 'get_netmask' usage: " )
+        ( >&2 echo -e "\tget_netmask <network_interface>")
         return 1
     fi
     
     ## Verifying interface passed exists
     OUTPUT_IFC=$(ifconfig $1 2> /dev/null)
     if [ ! "${OUTPUT_IFC}" ]; then
-        echo "[ *] Error - interface '$1' seems not to be present on the system"
+        ( >&2 echo "[ *] Error - interface '$1' seems not to be present on the system" )
         return 1
+    fi
+
+    ## At this point OUPUT_IFC contains the info from ifconfig for the network interface resquested
+    NETMASK_ADDR=$(echo ${OUTPUT_IFC} | \
+                 egrep -o 'Mask:[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}' | \
+                 cut -d: -f2 )
+    if [ ! "${NETMASK_ADDR}" ]; then
+        ( >&2 echo "[ *] No mask_addr" )
+        return 1
+    else    
+        echo ${NETMASK_ADDR}
     fi
 }
     
 ## Testing
-get_netmask eth1
+get_netmask 
+get_netmask eth0
+get_netmask wlan0
