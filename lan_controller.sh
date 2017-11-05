b691c323e3766bbc6bdc0ebee2326d1caff9b101
Arturo Ruiz Mañas <arruma2160@gmail.com>
2017-11-05 15:09:46 +0100

    lan controller encapsulation of code into functions

--- lan_controller.sh
+++ lan_controller.sh
@@ -1,14 +1,22 @@
 #! /bin/bash
 # Script to check what machines are reachable with a simple ping in your network
+function usage() {
+    echo "./lan_controller.sh"
+}
 
-while true; do 
-    echo "--------------------------" 
-    date 
-    echo
-    for pc in `seq 1 254`; do   
-        PC=$(ping -c 1 192.168.0.$pc | grep "Unreachable")
-        if [ ! "$PC" ]; then  
-            echo "192.168.0.$pc is alive"
-        fi
+function main() {
+    while true; do 
+        echo "--------------------------" 
+        echo "Starting new scanning at : $(date)"
+        echo
+        for pc in `seq 1 254`; do   
+            PC=$(ping -c 1 192.168.0.$pc | grep "Unreachable")
+            if [ ! "$PC" ]; then  
+                echo "192.168.0.$pc is alive"
+            fi
+        done
     done
-done
+}
+
+# Starting program
+main
