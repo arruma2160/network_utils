a708706956d132360bb381bc9870d4c82a7f3e11
Arturo Ruiz Mañas <arruma2160@gmail.com>
2017-12-26 11:05:10 +0100

    new test case and corrections

--- remove_name_spaces.sh
+++ remove_name_spaces.sh
@@ -28,13 +28,19 @@ function perform_recursive_action()
     ## Good use of function checks
     if [ $# != 2 ] && [ ! -d "$1" ] ; then
         printf "perform_recursive_action <directory>"
+    ## main functionality
     else
-        ## main functionality
         FROM=$(pwd)
-        cd $1
+        ## change argument folder if needed
+        aux=$(eval "$2" "$1")
+        if [ "${aux}" != "$1" ]; then 
+            mv "$1/" "${aux}/"
+        fi
+        ## Continue and recurse
+        cd "${aux}"
         ls | while read archive; do
             if [ -d "${archive}" ]; then
-                aux=$(eval $2 "${archive}")
+                aux=$(eval "$2" "${archive}")
                 if [ "${aux}" != "${archive}" ]; then 
                     mv "${archive}/" "${aux}/"
                 fi
@@ -56,7 +62,8 @@ function perform_recursive_action()
 # Script testing
 #
 
-function test_case() {
+function test_case() 
+{
     if [ "$2" != "$3" ]; then
         ## Error
         echo "0"
@@ -66,7 +73,8 @@ function test_case() {
     fi
 }
  
-function test_print_result() {
+function test_print_result() 
+{
     printf "[%d] Test: " "$1"
     if [ "$2" == "1" ]; then
         printf "passed -> $3 != $4 \n"
@@ -77,9 +85,12 @@ function test_print_result() {
 }
 
 
-function self_tests() {
+function self_tests() 
+{
     tests_passed=0
 
+
+    ## -----------------------------------
     test_number=1
     subs=$(subs_spaces "name with spaces")
     expected="name_with_spaces"
@@ -87,6 +98,8 @@ function self_tests() {
     test_print_result "${test_number}" "${rc}" "${subs}" "${expected}"
     tests_passed=$((${tests_passed} + ${rc}))
 
+
+    ## -----------------------------------
     test_number=2
     subs=$(subs_spaces " space at beginning")
     expected="_space_at_beginning"
@@ -94,6 +107,8 @@ function self_tests() {
     test_print_result "${test_number}" "${rc}" "${subs}" "${expected}"
     tests_passed=$((${tests_passed} + ${rc}))
 
+
+    ## -----------------------------------
     test_number=3
     subs=$(subs_spaces "space at end of name ")
     expected="space_at_end_of_name_"
@@ -102,6 +117,7 @@ function self_tests() {
     tests_passed=$((${tests_passed} + ${rc}))
 
 
+    ## -----------------------------------
     test_number=4
     subs=$(subs_spaces " space at start and end ")
     expected="_space_at_start_and_end_"
@@ -109,7 +125,8 @@ function self_tests() {
     test_print_result "${test_number}" "${rc}" "${subs}" "${expected}"
     tests_passed=$((${tests_passed} + ${rc}))
 
-    
+
+    ## -----------------------------------
     test_number=5
     expected="perform_recursive_action <directory>"
     result="$(perform_recursive_action)"
@@ -118,6 +135,7 @@ function self_tests() {
     tests_passed=$((${tests_passed} + ${rc}))
 
 
+    ## -----------------------------------
     test_number=6
     mkdir -p temporal && cd temporal && touch "uno dos tres"
     mkdir -p temp1 && cd temp1 && touch "cuatro cinco seis"
@@ -134,11 +152,34 @@ function self_tests() {
     if [ "${rc}" == 0 ]; then
         printf "Error at _%d\n" ${test_number}
     else
-        printf "[%d] Test: passed.\n" ${test_number}
+        printf "[%d] Test: passed -> %s\n" ${test_number} "corrected spaces at tree of folders"
         tests_passed=$((${tests_passed} + ${rc}))
     fi
     rm -rf temporal
 
+
+    ## -----------------------------------
+    test_number=7
+    mkdir "test 7"
+    cd "test 7"
+    touch "uno" "dos" "tres" "cuatro cinco"
+    cd ..
+    perform_recursive_action "test 7" subs_spaces
+    if [ -e "test_7" ] && [ -e "test_7/cuatro_cinco" ]; then
+        rc=1
+    else
+        rc=0
+    fi
+    if [ "${rc}" == 0 ]; then
+        printf "Error at _%d\n" ${test_number}
+    else
+        printf "[%d] Test: passed -> %s\n" ${test_number} "corrected spaces at folder argument"
+        tests_passed=$((${tests_passed} + ${rc}))
+    fi
+    rm -rf "test_7"
+
+
+    ## Final banner -> results
     printf "\nResult: %d/%d tests passed\n" "${tests_passed}" "${test_number}"
     if [ "${test_number}" != "${tests_passed}" ]; then
         printf "[!!] Errors detected at self_tests function\n"
