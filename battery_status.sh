e7147e8977c4712aff26ef9f54d6363ee23ec316
Arturo Ruiz Mañas <arruma2160@gmail.com>
2017-12-26 11:53:38 +0100

    added support for arch linux [battery status]

--- battery_status.sh
+++ battery_status.sh
@@ -4,10 +4,12 @@ ACPI=$(acpi)
 
 function test_acpi_dependencies() {
     if [ ! "$(which acpi)" ]; then
-        echo ""
-        echo "[*] $0: wants to install acpi dependencies"
-        echo ""
-        sudo apt-get install acpi
+        echo "[!!] $0: wants to install acpi dependencies"
+        if [ "$(uname -r | grep ARCH)" ]; then
+            sudo pacman -S acpi
+        else
+            sudo apt-get install acpi
+        fi
     fi
 }
 
@@ -15,8 +17,8 @@ function main_functionality() {
     if [ "$(echo $ACPI | grep Discharging)" ]; then
         REMAINING_PERCENT=$(echo $ACPI | cut -d, -f2)
         REMAINING_TIME=$(echo $ACPI | cut -d, -f3 | cut -d" " -f2)
-        echo " Battery:   $REMAINING_PERCENT"
-        echo " Time left:  $REMAINING_TIME"
+        echo "[-] Battery:   $REMAINING_PERCENT"
+        echo "[-] Time left:  $REMAINING_TIME"
     elif [ "$(echo $ACPI | grep Charging)" ]; then
         echo " Charging"
     elif [ "$(echo $ACPI | grep Full)" ]; then
